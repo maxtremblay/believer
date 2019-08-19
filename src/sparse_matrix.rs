0e9eea7547e7ba2049d3036617b8d97dea304528
Maxime <--replace-all>
2019-08-19 10:00:49 -0400

    Add transposition

--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -1,4 +1,6 @@
 use crate::ParityCheckMatrix;
+use itertools::Itertools;
+use std::cmp::Ordering;
 
 pub(crate) struct SparseMatrix<'a> {
     values: Vec<f64>,
@@ -8,6 +10,9 @@ pub(crate) struct SparseMatrix<'a> {
 
 impl<'a> SparseMatrix<'a> {
     pub(crate) fn from_parity_check(parity_check: &'a ParityCheckMatrix, values: Vec<f64>) -> Self {
+        if parity_check.len() != values.len() {
+            panic!("wrong number of values");
+        }
         Self {
             values,
             row_ranges: parity_check.row_ranges(),
@@ -30,6 +35,10 @@ impl<'a> SparseMatrix<'a> {
                 active: 0
             })
         })
+    }
+
+    pub(crate) fn values(&self) -> &[f64] {
+        &self.values
     }
 }
 
@@ -74,10 +83,50 @@ pub(crate) struct Transposer {
 
 impl Transposer {
     pub(crate) fn new(parity_check: &ParityCheckMatrix) -> Self {
-        unimplemented!()
+        let mut indices = Vec::with_capacity(parity_check.len());
+        let mut column_indices = Vec::with_capacity(parity_check.len());
+        let mut row_ranges = Vec::new();
+        row_ranges.push(0);
+
+        let mut active_col = 0;
+        let mut row_lenght = 0;
+
+        parity_check.positions_iter()
+            .enumerate()
+            .sorted_by(|(_, (r_0, c_0)), (_, (r_1, c_1))| {
+                match c_0.cmp(c_1) {
+                    Ordering::Equal => r_0.cmp(r_1),
+                    otherwise => otherwise
+                }
+            })
+            .for_each(|(idx, (row, col))| {
+                if col == active_col {
+                    row_lenght += 1;
+                } else {
+                    while active_col < col {
+                        active_col += 1;
+                        row_ranges.push(*row_ranges.last().unwrap_or(&0) + row_lenght); 
+                    }
+                    row_lenght = 1;
+                }
+                column_indices.push(row);
+                indices.push(idx);
+            });
+
+        row_ranges.push(*row_ranges.last().unwrap_or(&0) + row_lenght);
+
+        Transposer {
+            indices,
+            row_ranges,
+            column_indices,
+        }
     }
 
     pub(crate) fn transpose(&self, matrix: &SparseMatrix) -> SparseMatrix {
-        unimplemented!()
+        SparseMatrix {
+            values: self.indices.iter().map(|idx| matrix.values()[*idx].clone()).collect(),
+            row_ranges: &self.row_ranges,
+            column_indices: &self.column_indices,
+        }
     }
 }
