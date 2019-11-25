61032ee87bcd8ea98a345914fc1490f9931a3934
= <=>
2019-11-25 16:01:03 -0500

    Implement best code finding simulation until n events are found

--- src/generators/best_code_finder.rs
+++ src/generators/best_code_finder.rs
@@ -42,7 +42,19 @@ impl<'a, G: CodeGenerator> BestCodeFinderFromErasure<'a, G> {
             .with_n_iterations(n_iterations)
             .find_with_rng(rng)  
     }
+
+    pub fn find_best_code_simulating_until_n_events_are_found_with_rng<R: Rng>(
+        &self,
+        n_events: usize,
+        rng: &mut R,
+    ) -> (Option<ParityCheckMatrix>, SimulationResult) {
+        NEventsBestCodeFinderFromErasure::from(self)
+            .with_n_events(n_events)
+            .find_with_rng(rng)  
+    }
 }
+
+// The next 2 structs are basically the same things. They should be refactored.
 
 struct NIterationsBestCodeFinderFromErasure<'a, G: CodeGenerator> {
     code_finder: &'a BestCodeFinderFromErasure<'a, G>,
@@ -97,6 +109,59 @@ impl<'a, G: CodeGenerator> NIterationsBestCodeFinderFromErasure<'a, G> {
     }
 }
 
+struct NEventsBestCodeFinderFromErasure<'a, G: CodeGenerator> {
+    code_finder: &'a BestCodeFinderFromErasure<'a, G>,
+    n_events: usize,
+    random_seeds: Vec<u64>,
+}
+
+impl<'a, G: CodeGenerator> NEventsBestCodeFinderFromErasure<'a, G> {
+    fn from(code_finder: &'a BestCodeFinderFromErasure<'a, G>) -> Self {
+        Self { code_finder, n_events: 0, random_seeds: Vec::new() }
+    }
+
+    fn with_n_events(mut self, n_events: usize) -> Self {
+        self.n_events = n_events;
+        self
+    }
+
+    fn find_with_rng<R: Rng>(mut self, rng: &mut R) -> CodeAndResult {
+        self.initialize_random_seeds_with_rng(rng);
+        (0..self.code_finder.n_codes_to_try)
+            .into_par_iter()
+            .map(|code_index| {
+                let mut rng = self.get_rng_for(code_index);
+                self.simulate_one_code_with_rng(&mut rng)
+            })
+            .reduce(
+                || (None, SimulationResult::worse_result()),
+                |accumulator, code_and_result| Self::get_best_between(accumulator, code_and_result),
+        )
+    }
+
+    fn initialize_random_seeds_with_rng<R: Rng>(&mut self, rng: &mut R) {
+        self.random_seeds = rng
+            .sample_iter(Standard)
+            .take(self.code_finder.n_codes_to_try)
+            .collect()
+    }
+
+    fn get_rng_for(&self, index: usize) -> ChaCha8Rng {
+        ChaCha8Rng::seed_from_u64(self.random_seeds[index])
+    }
+
+    fn simulate_one_code_with_rng<R: Rng>(&self, rng: &mut R) -> CodeAndResult {
+        let code = self.code_finder.generator.generate_with_rng(rng);
+        let mut decoder = ErasureDecoder::with_prob(self.code_finder.erasure_prob).for_code(code);
+        let result = decoder.simulate_until_n_events_are_found_with_rng(self.n_events, rng);
+        (Some(decoder.take_code()), result)
+    }
+
+    fn get_best_between(first: CodeAndResult, second: CodeAndResult) -> CodeAndResult {
+        if first.1.is_better_than(&second.1) { first } else { second }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -105,7 +170,7 @@ mod test {
     use super::super::RegularLDPCCodeGenerator;
 
     #[test]
-    fn reproductibility_for_finding_best_ldpc_code_using_n_iterations() {
+    fn reproductibility_for_finding_best_ldpc_code_simulating_n_iterations() {
         let rng = ChaCha8Rng::seed_from_u64(123);
         let generator = RegularLDPCCodeGenerator::new(3, 4, 2, 4);
 
@@ -118,6 +183,24 @@ mod test {
         
         let code_and_result_1 = code_finder
             .find_best_code_simulating_n_iterations_with_rng(50, &mut rng.clone());
+
+        assert_eq!(code_and_result_0, code_and_result_1);
+    }
+
+    #[test]
+    fn reproductibility_for_finding_best_ldpc_code_simulating_until_n_events_are_found() {
+        let rng = ChaCha8Rng::seed_from_u64(123);
+        let generator = RegularLDPCCodeGenerator::new(3, 4, 2, 4);
+
+        let code_finder = BestCodeFinderFromErasure::from_generator(&generator)
+            .with_erasure_prob(0.5)
+            .among_n_codes(10);
+
+        let code_and_result_0 = code_finder
+            .find_best_code_simulating_until_n_events_are_found_with_rng(50, &mut rng.clone());
+        
+        let code_and_result_1 = code_finder
+            .find_best_code_simulating_until_n_events_are_found_with_rng(50, &mut rng.clone());
 
         assert_eq!(code_and_result_0, code_and_result_1);
     }
