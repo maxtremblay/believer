3dd37aa5b92a3361d73e51848fa90b59e5727261
Maxime Tremblay <matrem@protonmail.com>
2020-05-05 14:24:27 -0400

    Change project structure

--- src/generators/random_checks/mod.rs
+++
@@ -1,5 +0,0 @@
-//! A tool to generate checks for code construction.
-mod adjacency;
-
-pub mod generator;
-pub use generator::Generator;
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,4 @@
 //! A belief propapagation decoder for classical and quantum sparse error correcting codes.
-//! 
-pub mod channel;
-pub use channel::*;
 
-pub mod decoders;
-pub use decoders::*;
-
-pub mod gf2;
-pub use gf2::*;
-
-pub mod gf4_stabilizers;
-pub use gf4_stabilizers::*;
-
-// pub mod generators;
-// pub use generators::*;
-
-pub mod parity_check_matrix;
-pub use parity_check_matrix::*;
-
-pub mod paulis;
-pub use paulis::*;
-
-// mod sparse_matrix;
+pub mod classical;
+pub mod quantum;
